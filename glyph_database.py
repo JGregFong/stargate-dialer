c75bfe0aef84952977f0935bead3811c3be5f039
Jonathan Fong <jgregfong@gmail.com>
2026-05-20 14:25:48 -0700

    Added and updated the files to start main.py and a glyph_database.py.

---
+++ glyph_database.py
@@ -0,0 +1,17 @@
+glyphs =["Earth", "Crater", "Virgo", "Bootes", "Centaurus", "Libra", "Serpens Caput", "Norma", "Scorpio", "Cra", "Scutum", "Sagittarius",
+         "Aquila", "Mic", "Capricorn", "Pisces Austrinus", "Equuleus", "Aquarius", "Pegasus", "Sculptor", "Pisces", "Andromeda",
+         "Triangulum", "Aries", "Perseus", "Cetus", "Taurus", "Auriga", "Eridanus", "Orion", "Canis Minor", "Monoceros", "Gemini",
+         "Hydra", "Lynx", "Cancer", "Sextans", "Leo Minor", "Leo"]
+
+address_list = {
+    "Earth": [27, 26, 5, 36, 11, 28],
+    "Abydos": [26, 6, 14, 31, 11, 29],
+    "Atlantis": [18, 20, 1, 15, 14, 7, 19],
+    "Chulak": [8, 1, 22, 14, 36, 19],
+    "Othala": [10, 26, 22, 15, 32, 2, 8],
+    "Kheb": [25, 34, 5, 7, 22, 13],
+    "Castiana": [28, 2, 5, 8, 11, 15],
+    "Sahal": [28, 17, 18, 19, 20, 21],
+    "Vagon Brei": [2, 7, 1, 11, 18, 29]
+
+}
--- main.py
+++ main.py
@@ -1,5 +1,37 @@
+import glyph_database
+
+address_list = glyph_database.address_list
+glyphs = glyph_database.glyphs
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    dial_address("Earth")
 
 
 
-def main():
-    print("Hello World")
+
+
+def dial_address(address):
+    if address in address_list:
+        for symbol in address_list[address]:
+            print(symbol, glyphs[symbol])
+
+    else:
+        print("Invalid address")
+
+def manual_dial (number):
+    symbol_list =[]
+    if 9 >= number >= 7:
+        for symbol in range(number):
+            symbol_list.append(input(f"Enter symbol number {symbol}"))
+
+
+def check_address(address):
+    if address in address_list:
+        return True
+    else:
+        return False
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
